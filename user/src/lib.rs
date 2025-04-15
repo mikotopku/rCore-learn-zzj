b38f4bd1d73d0a58bd2ddefe36c865bddddc2f4b
zzj <2300011474@stu.pku.edu.cn>
2025-04-16 00:04:33 +0800

    ch2-apps

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -7,8 +7,8 @@ pub mod console;
 mod lang_items;
 mod syscall;
 
-#[no_mangle]
-#[link_section = ".text.entry"]
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text.entry")]
 pub extern "C" fn _start() -> ! {
     clear_bss();
     exit(main());
@@ -16,7 +16,26 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[linkage = "weak"]
-#[no_mangle]
+#[unsafe(no_mangle)]
 fn main() -> i32 {
     panic!("Cannot find main!");
+}
+
+fn clear_bss() {
+    unsafe extern "C" {
+        fn start_bss();
+        fn end_bss();
+    }
+    (start_bss as usize..end_bss as usize).for_each(|addr| unsafe {
+        (addr as *mut u8).write_volatile(0);
+    });
+}
+
+use syscall::*;
+
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+pub fn exit(exit_code: i32) -> isize {
+    sys_exit(exit_code)
 }
