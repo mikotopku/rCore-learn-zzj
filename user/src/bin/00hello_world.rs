4f46adcf552bf35317539f4c4a42f821b2c7d29b
zzj <2300011474@stu.pku.edu.cn>
2025-04-18 10:01:55 +0800

    ch3-coop-final

--- user/src/bin/00hello_world.rs
+++ user/src/bin/00hello_world.rs
@@ -1,11 +1,16 @@
 #![no_std]
 #![no_main]
 
+use user_lib::yield_;
+
 #[macro_use]
 extern crate user_lib;
 
 #[unsafe(no_mangle)]
 fn main() -> i32 {
-    user_lib::write(user_lib::console::STDOUT, b"hello\n");
+    println!("hello");
+    yield_();
+    println!("bonjour");
+    user_lib::console::print(format_args!("{} foo {:?}", 1, 2));
     0
 }
--- user/src/bin/02power.rs
+++ user/src/bin/02power.rs
@@ -2,12 +2,14 @@
 #![no_main]
 
 use user_lib::console;
+#[macro_use]
+use core::format_args;
 
 #[macro_use]
 extern crate user_lib;
 
 #[unsafe(no_mangle)]
 fn main() -> i32 {
-    user_lib::write(console::STDOUT, b"bonjour\n");
+    user_lib::write(console::STDOUT, b"what can i say \n");
     2
 }
